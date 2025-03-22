54b17ea8d6901b260734926af0d7125377aedde0
emtee40 <emtee40@users.noreply.github.com>
2025-03-22 02:33:37 -0500

    Add files via upload
    Upload of files

---
+++ Roguelike - With Spells.js
@@ -0,0 +1,235 @@
+class Dungeon {
+	constructor(width, height) {
+		this.width = width;
+		this.height = height;
+		this.map = this.generateMap();
+		this.player = new Player(1, 1, this);
+		this.enemies = this.spawnEnemies(5);
+		this.items = this.spawnItems(3);
+	}
+
+	generateMap() {
+		let map = [];
+		for (let y = 0; y < this.height; y++) {
+			let row = [];
+			for (let x = 0; x < this.width; x++) {
+				row.push(Math.random() > 0.2 ? '.' : '#'); // '.' = floor, '#' = wall
+			}
+			map.push(row);
+		}
+		return map;
+	}
+
+	spawnEnemies(count) {
+		let enemies = [];
+		for (let i = 0; i < count; i++) {
+			let x, y;
+			do {
+				x = Math.floor(Math.random() * this.width);
+				y = Math.floor(Math.random() * this.height);
+			} while (this.map[y][x] === '#' || (x === 1 && y === 1));
+			enemies.push(new Enemy(x, y));
+		}
+		return enemies;
+	}
+
+	spawnItems(count) {
+		let items = [];
+		for (let i = 0; i < count; i++) {
+			let x, y;
+			do {
+				x = Math.floor(Math.random() * this.width);
+				y = Math.floor(Math.random() * this.height);
+			} while (this.map[y][x] === '#' || (x === 1 && y === 1));
+			items.push(new Item(x, y, "Potion"));
+		}
+		return items;
+	}
+
+	display() {
+		let output = "";
+		for (let y = 0; y < this.height; y++) {
+			for (let x = 0; x < this.width; x++) {
+				if (this.player.x === x && this.player.y === y) {
+					output += '@';
+				} else if (this.enemies.some(e => e.x === x && e.y === y)) {
+					output += 'E';
+				} else if (this.items.some(i => i.x === x && i.y === y)) {
+					output += '!';
+				} else {
+					output += this.map[y][x];
+				}
+			}
+			output += '\n';
+		}
+		console.log(output);
+	}
+}
+
+class Player {
+	constructor(x, y, dungeon) {
+		this.x = x;
+		this.y = y;
+		this.hp = 10;
+		this.level = 1;
+		this.exp = 0;
+		this.inventory = [];
+		this.dungeon = dungeon;
+		this.spellTree = null; // Chosen spell category (Fire, Ice, Lightning, Poison)
+		this.spells = {}; // Unlocked spells
+	}
+
+	move(dx, dy) {
+		let newX = this.x + dx;
+		let newY = this.y + dy;
+		if (this.dungeon.map[newY][newX] !== '#') {
+			this.x = newX;
+			this.y = newY;
+			this.checkForItems();
+			this.checkForEnemies();
+		}
+	}
+
+	checkForItems() {
+		let itemIndex = this.dungeon.items.findIndex(i => i.x === this.x && i.y === this.y);
+		if (itemIndex !== -1) {
+			let item = this.dungeon.items.splice(itemIndex, 1)[0];
+			this.inventory.push(item);
+			console.log(`You picked up a ${item.name}!`);
+		}
+	}
+
+	checkForEnemies() {
+		let enemy = this.dungeon.enemies.find(e => e.x === this.x && e.y === this.y);
+		if (enemy) {
+			console.log("You encountered an enemy! Fighting...");
+			while (this.hp > 0 && enemy.hp > 0) {
+				enemy.hp -= 2;
+				console.log(`You hit the enemy! Enemy HP: ${enemy.hp}`);
+				if (enemy.hp > 0) {
+					this.hp -= 1;
+					console.log(`The enemy hits you! Your HP: ${this.hp}`);
+				}
+			}
+			if (this.hp > 0) {
+				console.log("You defeated the enemy!");
+				this.gainExp(10);
+				this.dungeon.enemies = this.dungeon.enemies.filter(e => e.hp > 0);
+			} else {
+				console.log("You died...");
+				process.exit();
+			}
+		}
+	}
+
+	gainExp(amount) {
+		this.exp += amount;
+		console.log(`You gained ${amount} XP! Total XP: ${this.exp}`);
+		while (this.exp >= this.level * 10) {
+			this.levelUp();
+		}
+	}
+
+	levelUp() {
+		this.level++;
+		this.hp += 5;
+		console.log(`You leveled up! Level: ${this.level}, HP: ${this.hp}`);
+
+		if (this.level === 4) {
+			this.chooseSpellTree();
+		} else if (this.level === 10 || this.level === 16) {
+			this.unlockNextSpellLevel();
+		} else if (this.level === 20) {
+			this.unlockAllSpells();
+		}
+	}
+
+	chooseSpellTree() {
+		console.log("Choose your spell tree: Fire, Ice, Lightning, or Poison.");
+		readline.question("Enter spell tree: ", (choice) => {
+			this.spellTree = choice.toLowerCase();
+			this.spells[this.spellTree] = 1;
+			console.log(`You have chosen the ${choice} spell tree! You can now cast ${choice} Level 1.`);
+			gameLoop();
+		});
+	}
+
+	unlockNextSpellLevel() {
+		if (this.spellTree) {
+			this.spells[this.spellTree] += 1;
+			console.log(`Your ${this.spellTree} spell is now Level ${this.spells[this.spellTree]}!`);
+		}
+	}
+
+	unlockAllSpells() {
+		console.log("You have reached level 20! All spells unlocked!");
+		this.spells = {
+			fire: 3,
+			ice: 3,
+			lightning: 3,
+			poison: 3
+		};
+	}
+
+	castSpell(spell) {
+		if (!this.spells[spell]) {
+			console.log("You don't know that spell.");
+			return;
+		}
+		let damage = this.spells[spell] * 3;
+		let enemy = this.dungeon.enemies.find(e => e.x === this.x && e.y === this.y);
+		if (enemy) {
+			enemy.hp -= damage;
+			console.log(`You cast ${spell} Level ${this.spells[spell]}! Enemy HP: ${enemy.hp}`);
+			if (enemy.hp <= 0) {
+				console.log("Enemy defeated!");
+				this.gainExp(10);
+				this.dungeon.enemies = this.dungeon.enemies.filter(e => e.hp > 0);
+			}
+		} else {
+			console.log("No enemy here.");
+		}
+	}
+}
+
+class Enemy {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.hp = 5;
+	}
+}
+
+class Item {
+	constructor(x, y, name) {
+		this.x = x;
+		this.y = y;
+		this.name = name;
+	}
+}
+
+// Game Loop
+const dungeon = new Dungeon(10, 10);
+console.log("Welcome to the dungeon! Use W, A, S, D to move.");
+dungeon.display();
+
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function gameLoop() {
+	readline.question("Move (W/A/S/D) or Cast (Fire/Ice/Lightning/Poison): ", (input) => {
+		input = input.toUpperCase();
+		if (["W", "A", "S", "D"].includes(input)) {
+			dungeon.player.move(input === "W" ? 0 : input === "S" ? 0 : input === "A" ? -1 : 1, input === "W" ? -1 : input === "S" ? 1 : 0);
+		} else {
+			dungeon.player.castSpell(input.toLowerCase());
+		}
+		console.clear();
+		dungeon.display();
+		gameLoop();
+	});
+}
+
+gameLoop();
